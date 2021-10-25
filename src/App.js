169d318c37e3787bdc7279cb7da99d7b9ced0170
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-10-24 22:43:04 -0300

    adicionado o style.css para o componente single

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Single from './components/Single'
+import Single from './components/Single';
+
+
+import './App.css';
 
 function App() {
   return (
     
     
-    <div className="App">     
+    <div className="App" style={{ 
+      backgroundImage: "url(/background.png)",
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: "cover",
+      height: "auto" }} >     
       <Single />
     </div>
     
--- src/components/Single/index.js
+++ src/components/Single/index.js
@@ -3,17 +3,18 @@ import iconSet from '../../assets/fonts/selection.json';
 import IcomoonReact, { iconList } from "icomoon-react";
 import terraPlana from '../../assets/images/terraPlana.png';
 
+import './style.css'
+
 function Single(){
     return(
-        <div className="single">
+        <div className="single container">
             <h1>A terra da Terra plana</h1>
             <img src={terraPlana} alt="" />
             <h2>Ouça agora o nosso novo single</h2>
             <ul className="links-single">
-                <li> <a href="#"> <IcomoonReact iconSet={iconSet} icon="spotify" />  </a> </li>
-                <li><a href="#"> <IcomoonReact iconSet={iconSet} icon="deezer" />  </a></li>
-                <li><a href="#"> <IcomoonReact iconSet={iconSet} icon="youtube" />  </a></li>
-
+                <li><a href="#"> <IcomoonReact color="#F2F2F2" size="4rem" iconSet={iconSet} icon="spotify" />  </a> </li>
+                <li><a href="#"> <IcomoonReact color="#F2F2F2" size="4rem" iconSet={iconSet} icon="deezer" />  </a></li>
+                <li><a href="#"> <IcomoonReact color="#F2F2F2" size="4rem" iconSet={iconSet} icon="youtube" />  </a></li>
             </ul>
         </div>
     );
