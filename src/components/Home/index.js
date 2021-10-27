eb20add4899b53325e99635926ac8fed4198975c
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-10-26 22:56:50 -0300

    adicionado o arquivo css do componente Home e o estilo para mobile

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,27 +1,77 @@
-import React from 'react';
-import iconSet from '../../assets/fonts/selection.json';
+import React from "react";
+import iconSet from "../../assets/fonts/selection.json";
 import IcomoonReact, { iconList } from "icomoon-react";
-import fotoHome from '../../assets/images/fotoHome.jpg';
-import logo from '../../assets/images/logoOnly.png'
+import fotoHome from "../../assets/images/fotoHome.jpg";
+import logo from "../../assets/images/logoOnly.png";
+
+import "./style.css";
 
 const Home = () => (
-    <div className="home container" id="home">
-        <img src={ logo } alt="Logo da banda Only" />
-        <div className="image">
-            <img src={fotoHome} alt="Foto da banda Only com o público pós show" />
-        </div>
-        <div className="social">
-        <ul className="links-single">
-                <li><a href="#"> <IcomoonReact color="#F2F2F2" size="4rem" iconSet={iconSet} icon="facebook" />  </a></li>
-                <li><a href="#"> <IcomoonReact color="#F2F2F2" size="4rem" iconSet={iconSet} icon="spotify" />  </a> </li>
-                <li><a href="#"> <IcomoonReact color="#F2F2F2" size="4rem" iconSet={iconSet} icon="youtube" />  </a></li>
-                <li><a href="#"> <IcomoonReact color="#F2F2F2" size="4rem" iconSet={iconSet} icon="instagram" />  </a></li>
-                <li><a href="#"> <IcomoonReact color="#F2F2F2" size="4rem" iconSet={iconSet} icon="deezer" />  </a></li>
-            </ul>
-
-        </div>
-
+  <div className="home" id="home">
+    <img className="logo" src={logo} alt="Logo da banda Only" />
+    <div className="image">
+      <img src={fotoHome} alt="Foto da banda Only com o público pós show" />
     </div>
-)
+    <div className="social">
+      <ul className="links">
+        <li>
+          <a href="#">
+            {" "}
+            <IcomoonReact
+              color="#F2F2F2"
+              size="3.75rem"
+              iconSet={iconSet}
+              icon="facebook"
+            />{" "}
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            {" "}
+            <IcomoonReact
+              color="#F2F2F2"
+              size="3.75rem"
+              iconSet={iconSet}
+              icon="spotify"
+            />{" "}
+          </a>{" "}
+        </li>
+        <li>
+          <a href="#">
+            {" "}
+            <IcomoonReact
+              color="#F2F2F2"
+              size="3.75rem"
+              iconSet={iconSet}
+              icon="youtube"
+            />{" "}
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            {" "}
+            <IcomoonReact
+              color="#F2F2F2"
+              size="3.75rem"
+              iconSet={iconSet}
+              icon="instagram"
+            />{" "}
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            {" "}
+            <IcomoonReact
+              color="#F2F2F2"
+              size="3.75rem"
+              iconSet={iconSet}
+              icon="deezer"
+            />{" "}
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
 
 export default Home;
