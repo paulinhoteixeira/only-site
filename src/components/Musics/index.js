3a8e5af7992b55a817fb269f8aad6af74d325366
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-11-04 23:05:03 -0300

    adicionado o arquivo css do componente Musics

--- src/components/Musics/index.js
+++ src/components/Musics/index.js
@@ -4,27 +4,34 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation, Mousewheel, Pagination } from "swiper";
 
 import "swiper/swiper-bundle.css";
+import './style.css'
+
 
 // install Swiper modules
 SwiperCore.use([Navigation, Mousewheel, Pagination]);
 
 const Musics = () => (
-  <div className="musics">
-    <Swiper 
-    direction={'horizontal'}
-    slidesPerView={1}
-    navigation={true} 
-    mousewheel={true}
-    pagination={{"clickable": true}} 
-    className="mySwiper">
-      {MusicsData.map((item) => {
-        return (
-          <SwiperSlide>
-            <img className="ep" src={item.image} alt={item.ep} />
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+  <div className="musics container">
+    <h1>Músicas</h1>
+    <h2>Eps e singles</h2>
+
+    <div className="eps">
+      <Swiper 
+      direction={'horizontal'}
+      slidesPerView={1}
+      navigation={true} 
+      mousewheel={true}
+      pagination={{"clickable": true}} 
+      className="mySwiper">
+        {MusicsData.map((item) => {
+          return (
+            <SwiperSlide>
+              <img className="ep" src={item.image} alt={item.ep} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
   </div>
 );
 
