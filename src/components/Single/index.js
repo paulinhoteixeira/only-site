43b3c4369a6430b5d136e79647ce621bcef9b23b
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-11-18 21:59:18 -0300

    ajuste no estilo do componente Single para mobile

--- src/components/Single/index.js
+++ src/components/Single/index.js
@@ -1,28 +1,61 @@
-import React from 'react';
-import iconSet from '../../assets/fonts/selection.json';
+import React from "react";
+import iconSet from "../../assets/fonts/selection.json";
 import IcomoonReact, { iconList } from "icomoon-react";
-import terraPlana from '../../assets/images/terraPlana.png';
+import terraPlanaTitulo from "../../assets/images/terraPlanaTitulo.png";
+import terraPlana from "../../assets/images/terraPlana.png";
 
-import './style.css'
+import "./style.css";
 
-function Single(){
-    return(
-        <div className="single section">
-            <div className="container">
-                <h1>A terra da Terra plana</h1>
-                <img src={terraPlana} alt="" />
-                <h2>Ouça agora o nosso novo single</h2>
-                <ul className="links-single">
-                    <li><a href="#"> <IcomoonReact color="#F2F2F2" size="4rem" iconSet={iconSet} icon="spotify" />  </a> </li>
-                    <li><a href="#"> <IcomoonReact color="#F2F2F2" size="4rem" iconSet={iconSet} icon="deezer" />  </a></li>
-                    <li><a href="#"> <IcomoonReact color="#F2F2F2" size="4rem" iconSet={iconSet} icon="youtube" />  </a></li>
-                </ul>
-                <h3>Ir para o site</h3>
-
-            </div>
+function Single() {
+  return (
+    <div className="section" id="single">
+      <div className="container grid">
+        <div className="single-title">
+          <img src={terraPlanaTitulo} alt="" />
         </div>
-    );
-    
+        <div className="single-image">
+          <img src={terraPlana} alt="" />
+        </div>
+        <h2>Ouça agora o nosso novo single</h2>
+        <ul className="links-single">
+          <li>
+            <a href="#">
+              {" "}
+              <IcomoonReact
+                color="#F2F2F2"
+                size="4rem"
+                iconSet={iconSet}
+                icon="spotify"
+              />{" "}
+            </a>{" "}
+          </li>
+          <li>
+            <a href="#">
+              {" "}
+              <IcomoonReact
+                color="#F2F2F2"
+                size="4rem"
+                iconSet={iconSet}
+                icon="deezer"
+              />{" "}
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              {" "}
+              <IcomoonReact
+                color="#F2F2F2"
+                size="4rem"
+                iconSet={iconSet}
+                icon="youtube"
+              />{" "}
+            </a>
+          </li>
+        </ul>
+        <h3>Ir para o site</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Single;
