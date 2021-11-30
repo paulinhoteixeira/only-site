d0ed0cb9cb954dcc2005f2e55e6e9b86be174082
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-11-29 23:00:41 -0300

    estilo para o componente single para dispositivos maiores que 1200px

--- src/components/Single/index.js
+++ src/components/Single/index.js
@@ -11,48 +11,54 @@ function Single() {
   return (
     <div id="single">
       <div className="container grid">
-        <div className="single-title">
-          <img src={terraPlanaTitulo} alt="" />
+          <div className="single-title">
+            <img src={terraPlanaTitulo} alt="" />
+          </div>
+        
+          <div className="single-image">
+            <img src={terraPlana} alt="" />
+          </div>
+        
+
+        <div className="single-listen-to">
+          <h2>Ouça agora o nosso novo single</h2>
+          <ul className="links-single">
+            <li>
+              <a href="#">
+                {" "}
+                <IcomoonReact
+                  color="#F2F2F2"
+                  size="4rem"
+                  iconSet={iconSet}
+                  icon="spotify"
+                />{" "}
+              </a>{" "}
+            </li>
+            <li>
+              <a href="#">
+                {" "}
+                <IcomoonReact
+                  color="#F2F2F2"
+                  size="4rem"
+                  iconSet={iconSet}
+                  icon="deezer"
+                />{" "}
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                {" "}
+                <IcomoonReact
+                  color="#F2F2F2"
+                  size="4rem"
+                  iconSet={iconSet}
+                  icon="youtube"
+                />{" "}
+              </a>
+            </li>
+          </ul>
+
         </div>
-        <div className="single-image">
-          <img src={terraPlana} alt="" />
-        </div>
-        <h2>Ouça agora o nosso novo single</h2>
-        <ul className="links-single">
-          <li>
-            <a href="#">
-              {" "}
-              <IcomoonReact
-                color="#F2F2F2"
-                size="4rem"
-                iconSet={iconSet}
-                icon="spotify"
-              />{" "}
-            </a>{" "}
-          </li>
-          <li>
-            <a href="#">
-              {" "}
-              <IcomoonReact
-                color="#F2F2F2"
-                size="4rem"
-                iconSet={iconSet}
-                icon="deezer"
-              />{" "}
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              {" "}
-              <IcomoonReact
-                color="#F2F2F2"
-                size="4rem"
-                iconSet={iconSet}
-                icon="youtube"
-              />{" "}
-            </a>
-          </li>
-        </ul>
         <Link to="/home"><h3>Ir para o site</h3></Link>
         
       </div>
